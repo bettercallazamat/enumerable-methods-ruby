028152444ab74cb3241373fbdaf5ffa371766ed0
bettercallazamat <bettercallazamat@gmail.com>
2020-03-26 02:14:37 +0600

    Add my_each_with_index method

--- enumerable.rb
+++ enumerable.rb
@@ -1,19 +1,22 @@
 module Enumerable
-
-def my_each
-  self.lenght.times do |i|
-    yield(self[i])
+  def my_each
+    self.length.times do |i| 
+      if block_given?
+        yield(self[i])
+      else
+        break
+      end
+    end
   end
-end
+
+  def my_each_with_index
+    self.length.times do |i|
+      if block_given?
+        yield(self[i], i)
+      else
+        break
+      end
+    end
+  end
 
 end
-
-
-test_array1 = [11, 2, 3, 56]
-test_array2 = %w[a b c d]
-​
-# my_each
-p 'my_each'
-test_array1.my_each { |x| p x }
-test_array2.my_each { |x| p x }
-p test_array2.my_each
